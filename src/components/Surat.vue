80803b7eaa0f3887eb21ca9735c99d9341d8598b
Khuirul-Huda <hudah2102@gmail.com>
2022-01-08 16:07:08 +0700

    feat: created surat.vue for main page component

---
+++ src/components/Surat.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="card-like">
+    <div class="surah-wrapper flex-sb">
+      <h3 class="surah-name nfont">{{ surat }}</h3>
+      <p class="name-short-arabic">{{ surat }}</p>
+    </div>
+    <div class="more-info">
+      <p class="arti nfont">({{ arti }})</p>
+      <div>
+        <div class="flex-start">
+          <p class="revelation-arabic">{{ revelation }}</p>
+          <p class="verses nfont">{{ verses }} ayat</p>
+          <!-- <p class="number-sr nfont">
+          {{ke}}
+        </p>-->
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Surat",
+  props: {
+    surat: String, //surat
+    arabic: String, //$surat => arabic
+    arti: String, //arti $surat
+    revelation: String, //diturunkan di
+    verses: Number, //berapa ayat
+    ke: Number, //surat ke...
+  },
+};
+</script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Quicksand&display=swap");
+
+.card-like {
+  background: linear-gradient(
+    110.5deg,
+    rgba(248, 196, 249, 0.66) 22.8%,
+    rgba(253, 122, 4, 0.15) 64.6%
+  );
+  padding: 25px;
+  border-radius: 25px;
+  margin: 5%;
+  width: 230px;
+  height: 170px;
+}
+
+.nfont {
+  font-family: "Quicksand", sans-serif;
+}
+
+.flex-sb {
+  display: flex;
+  justify-content: space-between;
+}
+
+.name-short-arabic {
+  font-size: 140%;
+}
+
+.flex-start {
+  display: flex;
+  justify-content: center;
+}
+
+.revelation-arabic {
+  font-size: 120%;
+  margin-right: 10%;
+}
+
+.number-sr {
+  margin-left: 10%;
+  font-size: 100%;
+}
+
+.flex-end {
+  display: flex;
+  justify-content: flex-end;
+}
+
+.arti {
+  display: flex;
+  justify-content: center;
+}
+</style>
