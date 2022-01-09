26d1efb7f9c00ad9ef8248640c7a22ad504d5e56
Khuirul-Huda <hudah2102@gmail.com>
2022-01-09 20:00:52 +0700

    created Main.vue for homepage

---
+++ src/views/Main.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p></p>
+
+    <DaftarSurat />
+  </div>
+</template>
+
+<script>
+import DaftarSurat from "../views/DaftarSurat.vue";
+
+export default {
+  name: "Main",
+  components: {
+    DaftarSurat,
+  },
+};
+</script>
