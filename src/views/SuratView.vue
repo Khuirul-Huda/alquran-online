97b585139a35bf5134f62842b9c31a2ef9cfc8ce
Khuirul-Huda <hudah2102@gmail.com>
2022-04-06 08:24:31 +0700

    params validation

--- src/views/SuratView.vue
+++ src/views/SuratView.vue
@@ -5,5 +5,22 @@
 <script>
 export default {
   name: "SuratView",
+  data() {
+    return {
+      surahnumber: 0,
+    };
+  },
+  mounted() {
+    const params = this.$route.params.surat;
+    if (isNaN(params)) {
+      this.$router.push({ name: "Main" });
+    } else {
+      if (params < 1 || params > 114) {
+        this.$router.push({ name: "Main" });
+      } else {
+        this.surahnumber = this.$route.params.surat;
+      }
+    }
+  },
 };
 </script>
