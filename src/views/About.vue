4fb36d294224c43c50a663343add2a35715af2c2
Khuirul-Huda <hudah2102@gmail.com>
2022-06-13 15:07:53 +0700

    update about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,38 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Tentang</h1>
+    <div class="content">
+      Aplikasi "Al-Quran Online", mempunyai ayat-ayat yang lengkap dan tafsir
+      serta terjemahan. Aplikasi ini online 24/7 selama server tidak mengalami
+      masalah atau gangguan teknis. Data di dalam aplikasi ini ( tafsir, ayat,
+      terjemahan ) bersumber dari API ->
+      <a href="https://github.com/gadingnst/quran-api">gadingnst/quran-api.</a>
+      dan ditransfer melalui CDN sehingga mampu menangani ratusan sampai ribuan
+      request setiap harinya. Aplikasi ini bersifat <i>open source</i>, kode
+      sumber bisa dilihat dan/atau didownload dari
+      <a href="https:///github.com/Khuirul-Huda/alquran-online"
+        >Khuirul-Huda/alquran-online</a
+      >.
+    </div>
+    <h2>Menemukan Masalah?</h2>
+    <div class="content">
+      Laporkan kesalahan teknis aplikasi atau kesalahan terjemahan, ayat, tafsir
+      melalui DM.
+      <br />
+      Atau buat <i>Issue</i> baru
+      <a href="https:///github.com/Khuirul-Huda/alquran-online/issues"
+        >disini</a
+      >
+    </div>
+    <h2>Hubungi Saya</h2>
+    <a href="https://instagram.com/khuirul_huda">@khuirul_huda</a>
+    <h6>2022</h6>
   </div>
 </template>
+
+<style scoped>
+.content {
+  margin-left: 10%;
+  margin-right: 10%;
+}
+</style>
